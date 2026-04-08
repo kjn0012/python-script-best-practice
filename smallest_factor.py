51457eee9e86422d915767299b88fd0ee962ee36
kjn0012 <kjn0012@auburn.edu>
2026-04-08 13:23:42 -0500

    Corrected indent error, function definition, over ride of function name, and undefined variable "result".

--- smallest_factor.py
+++ smallest_factor.py
@@ -28,18 +28,12 @@ def smallest_prime_factor(n):
 
 print("Step 5: Compute and print result")
 
-if __name__ == '__main__':
-    if len(sys.argv) != 2:
-        sys.exit(sys.argv[0] + ": Expecting one command line argument -- the integer for which to get the smallest factor")
-    n = int(sys.argv[1])
-    if n < 1:
-        sys.exit(sys.argv[0] + ": Expecting a positive integer")
-    
-    smallest_prime_factor = get_smallest_prime_factor(n)
-    
-    if smallest_prime_factor is None:
+result = smallest_prime_factor(n)
+
+print("Step 6: Results")
+if result is None:
     print(f"No factors found. {n} is prime.")
-        print(n)
-    else:
+    print(n)
+else:
     print(f"Smallest factor of {n} is {result}")
-        print(smallest_prime_factor)
+    print(result)
